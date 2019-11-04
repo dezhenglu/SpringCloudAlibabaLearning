038e3824e5270c2c06ec280c6305979e6b2ba6d3
ludezheng <ludezheng@bsoft.com.cn>
2019-11-05 00:24:30 +0800

    将应用注册到nacos,实现引用(内容中心->用户中心)

--- centent-center/src/main/java/online/ludzh/contentcenter/TestController.java
+++ centent-center/src/main/java/online/ludzh/contentcenter/TestController.java
@@ -3,6 +3,8 @@ package online.ludzh.contentcenter;
 import online.ludzh.contentcenter.domain.dao.content.ShareMapper;
 import online.ludzh.contentcenter.domain.entity.content.Share;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,6 +20,9 @@ public class TestController {
     @Autowired
     ShareMapper shareMapper;
 
+    @Autowired
+    private DiscoveryClient discoveryClient;
+
     @GetMapping("/test")
     public List<Share> testInsert(){
         Share share = new Share();
@@ -32,5 +37,25 @@ public class TestController {
         // 2. 做查询; 查询当权数据库所有的share select * from share;
         List<Share> shares = this.shareMapper.selectAll();
         return shares;
+    }
+
+    /**
+     * 测试: 服务发现,证明内容中心总能找到用户中心
+     * @return 用户中心所有实例的地址信息
+     */
+    @GetMapping("/test2")
+    public List<ServiceInstance> getInstances(){
+        // 查询指定服务的所有实例的信息
+        return this.discoveryClient.getInstances("user-center");
+    }
+
+    /**
+     * 测试: 查询已经出册的服务列表
+     * @return 注册中心注册的服务列表
+     */
+    @GetMapping("/test3")
+    public List<String> getServices(){
+        // 查询指定服务的所有实例的信息
+        return this.discoveryClient.getServices();
     }
 }
--- centent-center/src/main/java/online/ludzh/contentcenter/service/content/ShareService.java
+++ centent-center/src/main/java/online/ludzh/contentcenter/service/content/ShareService.java
@@ -1,19 +1,25 @@
 package online.ludzh.contentcenter.service.content;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import online.ludzh.contentcenter.domain.dao.content.ShareMapper;
 import online.ludzh.contentcenter.domain.dto.content.ShareDTO;
 import online.ludzh.contentcenter.domain.dto.user.UserDTO;
 import online.ludzh.contentcenter.domain.entity.content.Share;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.List;
+
 /**
  * Created by mi-ludzh on 0003 2019/11/3.
  */
+@Slf4j
 @Service
 @RequiredArgsConstructor(onConstructor = @__(@Autowired))
 public class ShareService {
@@ -22,14 +28,23 @@ public class ShareService {
 
     private final RestTemplate restTemplate;
 
+    private final DiscoveryClient discoveryClient;
+
     public ShareDTO findById(Integer id){
         // 获取分享详情
         Share share = this.shareMapper.selectByPrimaryKey(id);
         // 发布人id
         Integer userId = share.getUserId();
 
+        List<ServiceInstance> instances = discoveryClient.getInstances("user-center");
+
+        String targetURL = instances.stream()
+                .map(instance -> instance.getUri().toString() + "/users/{id}")
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("当前没有实例"));
+        log.info("请求的目标地址:{}", targetURL);
         UserDTO userDTO = this.restTemplate.getForObject(
-                "http://localhost:8080/users/{id}",
+                targetURL,
                 UserDTO.class, userId
         );
 
